ef4e4c738e8219952667368f20e7204a0de59e0c
jgodfrey92 <60187478+jgodfrey92@users.noreply.github.com>
2020-02-03 20:28:30 -0500

    Add files via upload

--- money.cpp
+++ money.cpp
@@ -1,17 +1,18 @@
 /********
- Author:
+ Author: Joshua Godfrey
  Program: Money class - program assignment #1
- Due Date:
- Purpose:
+ Due Date: 2/3/2019
+ Purpose: This program handles conversion between different currency formats
  ********/
 #include "money.h"
 
 // convert current currency values to a string
 string Money::toString() {
-    return "";
+    return to_string(hundreds) + " hundred(s) " + to_string(tens) + " ten(s) " + to_string(fives) + " five(s) " + to_string(ones) + " one(s) " + to_string(quarters) + " quarter(s) " + to_string(dimes) + " dime(s) " + to_string(nickels) + " nickel(s) " + to_string(cents) + " penny/ies";
 }
 
 // format amount as a local currency and return
+
 string Money::toCurrency(double amount) {
     stringstream ss;
 
@@ -23,14 +24,21 @@ string Money::toCurrency(double amount) {
 // read currency values from stdin and compute value
 // return results
 string Money::processChange() {
-    return "";
+	cin >> hundreds >> tens >> fives >> ones >> quarters >> dimes >> nickels >> cents;
+	convertChange();
+
+    return toString() + " = " + toCurrency(total);
 }
 
 // read from stdin and process float command
 // convert float to change
 // return results
 string Money::processFloat() {
-    return "";
+    cin >> total;
+    double temp = total;
+    convertFloat(total);
+
+    return toCurrency(temp) + " = " + toString();
 }
 
 // read from stdin and process check command
@@ -46,7 +54,18 @@ string Money::processCheck() {
 // calculate difference and compute change values
 // return the results
 string Money::processChangeFloat() {
-  return "";
+	double paidAmount, cost, changeAmount;
+	double temp;
+	cin >> paidAmount >> cost;
+	temp = paidAmount;
+	changeAmount = paidAmount - cost;
+
+	convertFloat(changeAmount);
+	stringstream ss;
+	ss.precision(2);
+	ss << fixed << "change back on $" << temp << " for a purchase of $" << cost << " is $" << changeAmount << " which is " << toString();
+
+	return ss.str();
 }
 
 // read from stdin and process change-change command
@@ -55,5 +74,46 @@ string Money::processChangeFloat() {
 // calculate difference and compute change values
 // output the results
 string Money::processChangeChange() {
-  return "";
+    double cost, changeAmount;
+    string temp;
+    cin >> hundreds >> tens >> fives >> ones >> quarters >> dimes >> nickels >> cents >> cost;
+    temp = toString();
+	convertChange();
+
+	changeAmount = total - cost;
+	convertFloat(changeAmount);
+
+	stringstream ss;
+	ss.precision(2);
+	ss << fixed << "change back on " << temp << " for a purchase of $" << cost << " is $" << changeAmount << " which is " + toString();
+	return ss.str();
 }
+
+void Money::convertFloat(double amount)
+{
+		//This converts the double amount into pennies and then grabs each denomination in descending order
+		amount *= 100;
+	    long int temp = amount; //couldn't quite get casting to work so I am just assigning the type I need to a new variable instead
+	    hundreds = temp / 10000;
+	    temp %= 10000;
+	    tens = temp / 1000;
+	    temp %= 1000;
+	    fives = temp / 500;
+	    temp %= 500;
+	    ones = temp / 100;
+	    temp %= 100;
+	    quarters = temp / 25;
+	    temp %= 25;
+	    dimes = temp / 10;
+	    temp %= 10;
+	    nickels = temp / 5;
+	    temp %= 5;
+	    cents = temp;
+}
+
+void Money::convertChange()
+{
+	
+	total = (hundreds * 100) + (tens * 10) + (fives * 5) + ones + (quarters * 0.25) + (dimes * 0.10) + (nickels * 0.05) + (cents * 0.01);
+}
+
